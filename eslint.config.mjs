47b1b9f061d9af5f6b3d4cf3049429a11a0d3723
daveyeb <daveyeb@gmail.com>
2024-12-16 14:27:11 -0500

    add lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,13 +6,12 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  {
+  { languageOptions: { globals: globals.browser,
     parserOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
     }
-  },
+   } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
